e0c431fb45c54c51abd319755154c7d4cdc13113
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-12-14 15:56:38 -0500

    change githooks to exclude karma pre push

--- grunt/githooks.js
+++ grunt/githooks.js
@@ -1,6 +1,6 @@
 module.exports = {
     all: {
-        'pre-commit': 'jshint jsbeautifier:pre-commit',
+        'pre-commit': 'jshint jsbeautifier:pre-commit'
         // 'pre-push': 'karma:unit',
         // 'post-merge': 'shell:npm-install'
     }
