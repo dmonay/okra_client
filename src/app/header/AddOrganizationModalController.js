3bbaf661589c4425fa9105b25b944ad7b952e5e3
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-12-14 20:13:02 -0500

    modal service now closes

--- src/app/header/AddOrganizationModalController.js
+++ src/app/header/AddOrganizationModalController.js
@@ -3,13 +3,22 @@
 
     var app = angular.module('HeaderModule');
 
-    function AddOrganizationModalController($scope, OrganizationFactory, $modal) {
+    function AddOrganizationModalController($scope, OrganizationFactory, $modalInstance) {
         var modal = this;
 
-
+        modal.createOrganization = function (name) {
+            if (modal.organizationForm.$valid) {
+                OrganizationFactory.createOrganization(name).then(function (response) {
+                    console.log(response);
+                });
+            }
+        };
+        modal.closeModal = function () {
+            $modalInstance.close();
+        };
     }
 
-    AddOrganizationModalController.$inject = ['$scope', 'OrganizationFactory'];
+    AddOrganizationModalController.$inject = ['$scope', 'OrganizationFactory', '$modalInstance'];
 
     app.controller('AddOrganizationModalController', AddOrganizationModalController);
 
