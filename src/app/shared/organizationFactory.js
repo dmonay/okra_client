c00ed022f19b2eeb3505c0c1c38b49beb848b3f5
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-12-14 21:01:04 -0500

    hard coded user id

--- src/app/shared/organizationFactory.js
+++ src/app/shared/organizationFactory.js
@@ -7,7 +7,8 @@
         var organizationAPI = {
             createOrganization: function (orgName) {
                 return $http.post(okraAPI.createOrg, {
-                    organization: orgName
+                    organization: orgName,
+                    userId: "548e3feebee23fc7375b788b"
                 });
             }
         };
