dcb6080f8d150781f2f1e308e2c09c4278c2684d
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-12-19 16:35:52 -0500

    added ui routing capability for organizations

--- src/app/app-route.js
+++ src/app/app-route.js
@@ -1,36 +1,16 @@
-// (function () {
-//     'use strict';
+(function () {
+    'use strict';
 
-//     var app = angular.module('okra.routes', ['ui.router']);
+    var router = angular.module('okra.routes', ['ui.router']);
 
-//     app.config(function ($stateProvider, $urlRouterProvider) {
+    router.config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
 
-//         $urlRouterProvider.otherwise(function ($injector, $location) {
-//             var storePath = $location.path().split('/').slice(0, 2).join('/');
-//             return storePath + '/error/404/?path=' + $location.$$path;
-//         });
-
-//         // Make trailing slash optional
-//         $urlRouterProvider.rule(function ($injector, $location) {
-//             var path = $location.path(),
-//                 search = $location.search(),
-//                 params;
-
-//             if (path[path.length - 1] === '/') {
-//                 return;
-//             }
-
-//             if (Object.keys(search).length === 0) {
-//                 return path + '/';
-//             }
-
-//             params = [];
-//             angular.forEach(search, function (v, k) {
-//                 params.push(k + '=' + v);
-//             });
-
-//             return path + '/?' + params.join('&');
-//         });
-
-//     });
-// })();
+        $stateProvider
+            .state('organization', {
+                url: '/organization/:organization',
+                templateUrl: 'app/organization/organization-tree.tpl.html',
+                controller: 'OrganizationController as vm'
+            });
+    });
+})();
