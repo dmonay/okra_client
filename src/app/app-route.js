ec3d36d46ced770308589ca7eab068e33885ce0e
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-12-31 05:09:01 -0500

    added new login route and template

--- src/app/app-route.js
+++ src/app/app-route.js
@@ -7,6 +7,11 @@
         $urlRouterProvider.otherwise('/');
 
         $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'app/login/login.tpl.html',
+                // controller: 'LoginController as vm'
+            })
             .state('organization/tree', {
                 url: '/organization/:organization/tree/:treeId',
                 templateUrl: 'app/organization/organization-tree.tpl.html',
