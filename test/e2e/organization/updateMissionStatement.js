f6c7f9a313a4e2780ab039d49be07f8212e1c00e
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-12-31 03:55:31 -0500

    e2e test for updating missions for orgs

---
+++ test/e2e/organization/updateMissionStatement.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('Adding an Organization', function() {
+    var ptor;
+    var modal;
+    var openModalBtn;
+
+    beforeEach(function() {
+        ptor = protractor.getInstance(); //gets the instance of protractor
+
+        browser.get('http://localhost:3333/#/organization/someorg/trees');
+
+        ptor.waitForAngular(); //waits for angular to bootstrap
+
+        openModalBtn = element(by.css('[ng-click="vm.openMissionStatementModal()"]'));
+    });
+
+    it('Should open the modal and close the modal', function() {
+        modal = element(by.css('.md-dialog-container'));
+        expect(modal.isPresent()).toBeFalsy();
+        openModalBtn.click();
+        expect(modal.isPresent()).toBeTruthy();
+        element(by.css('[ng-click="modal.closeModal()"]')).click();
+        expect(modal.isPresent()).toBeFalsy();
+    });
+
+    it('Should validate if the fields are incorrect', function() {
+        openModalBtn.click();
+        var form = element(by.css('.form-horizontal')),
+        	errorMsg = form.element(by.css('.error-msg')),
+        	missionStatementInput = form.element(by.model('modal.newMissionStatement')),
+        	submitFormBtn = element(by.css('[ng-click="modal.saveMissionStatement()"]'));
+
+      	submitFormBtn.click();
+      	expect(browser.helpers.hasClass(errorMsg, 'ng-hide')).toBeFalsy();
+
+      	missionStatementInput.sendKeys('Some Mission Statement');
+        expect(browser.helpers.hasClass(errorMsg, 'ng-hide')).toBeTruthy();
+      	submitFormBtn.click();
+        expect(element(by.css('.md-dialog-container')).isPresent()).toBeFalsy();
+    });
+
+});
