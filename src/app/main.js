3cff0c2c96259bfd635111a94bf0d72447faa654
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-01-03 04:21:19 -0500

    added documentation for memberservice

--- src/app/main.js
+++ src/app/main.js
@@ -515,8 +515,30 @@
     'use strict';
 
     var app = angular.module('SharedServices');
+    /**
+     * @ngdoc service
+     * @name SharedServices.MemberService
+     * @description
+     *
+     * # MemberService
+     * Interacts with member data.
+     *
+     */
 
     function MemberService() {
+
+        /**
+         * @ngdoc method
+         * @name createUser
+         * @description Creates a new member object.
+         * @methodOf SharedServices.MemberService
+         * @param {string}
+         *     username for the new member.
+         * @param {string}
+         *     role for the new member.
+         * @returns {Object} An object containing a username and a role for that member.
+         */
+
         function Member(username, role) {
             this.userName = username;
             this.role = role;
