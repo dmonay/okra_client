c13702391c3ce50cbef0bbd0afdcc0ffdb9a577d
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-01-05 00:09:06 -0500

    addTreeModalController has been properly unit tested

---
+++ src/app/tree/addTreeModalController.spec.js
@@ -0,0 +1,92 @@
+'use strict';
+describe('AddTreeModalController', function () {
+    var scope;
+    var modalController;
+    var element;
+    var $httpBackend;
+
+    beforeEach(function () {
+        module('okra');
+    });
+
+    beforeEach(inject(function ($controller, $rootScope, _$http_, TreeFactory, _$httpBackend_,
+        $mdDialog, $compile, _hardCoded_) {
+        scope = $rootScope.$new();
+        $httpBackend = _$httpBackend_;
+
+        //Mock out the modal
+        modalController = $controller('AddTreeModalController', {
+            $scope: scope,
+            TreeFactory: TreeFactory,
+            $mdDialog: $mdDialog,
+            hardCoded: _hardCoded_
+        });
+
+        element = angular.element(
+            '<form name="addTreeForm">' +
+            '<md-input-group>' +
+            '<label>Tree Name</label>' +
+            '<md-input required name="newTreeName" ng-model="newTreeName" autocapitalize="off"></md-input>' +
+            '</md-input-group>' +
+            '<md-radio-group class="horizontal-radio-group" layout="row" ng-model="timeframe">' +
+            '<md-radio-button value="monthly" aria-label="Monthly"> Monthly </md-radio-button>' +
+            '<md-radio-button value="yearly" artial-label="Yearly"> Yearly </md-radio-button>' +
+            '</md-radio-group>' +
+            '</form>'
+        );
+
+        $compile(element)(scope);
+        scope.$digest();
+
+        modalController.addTreeForm = scope.addTreeForm;
+    }));
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+
+    describe('Add a Tree', function () {
+
+        it('Should instantiate up the controller for the modal', function () {
+            expect(modalController).not.toBeUndefined();
+        });
+
+        it('Should validate properly if fields are not filled in', function () {
+            expect(modalController.addTreeForm.$valid).toBeFalsy();
+            expect(modalController.formSubmitted).toBeFalsy();
+            modalController.addTree();
+            expect(modalController.formSubmitted).toBeTruthy();
+        });
+
+        it('Should validate properly if fields are correct and filled in', function () {
+            expect(modalController.addTreeForm.$valid).toBeFalsy();
+            modalController.addTreeForm.newTreeName.$setViewValue('testTree');
+            modalController.newTreeName = 'testTree';
+            expect(modalController.addTreeForm.$valid).toBeTruthy();
+        });
+
+        it('Should send the right data to the backend', function () {
+            $httpBackend.expectPOST('http://localhost:8080/create/tree/someorg', {
+                    treeName: 'testTree',
+                    timeframe: 'monthly',
+                    userId: '5491e2aebee23fc7375b789c',
+                    username: 'john234'
+                })
+                .respond(200, {
+                    message: 'Success',
+                    tree: 'testTree'
+                });
+
+            modalController.addTreeForm.newTreeName.$setViewValue('testTree');
+            modalController.newTreeName = 'testTree';
+
+            modalController.timeframe = 'monthly';
+
+            modalController.addTree();
+            $httpBackend.flush();
+        });
+
+    });
+});
