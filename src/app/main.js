094e5db919be610e764c5d984189533f6d0d3ab5
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-01-05 11:59:24 -0500

    added documentation for organizationFactory

--- src/app/main.js
+++ src/app/main.js
@@ -328,16 +328,45 @@
     'use strict';
 
     var app = angular.module('SharedFactories');
+    /**
+     * @ngdoc service
+     * @name SharedFactories.OrganizationFactory
+     * @description
+     *
+     * # OrganizationFactory
+     * A factory that holds methods that interact with the backend API, specifically organization data endpoints.
+     *
+     */
 
     function OrganizationFactory($http, okraAPI, hardCoded) {
 
         var organizationAPI = {
+            /**
+             * @ngdoc method
+             * @name createOrganization
+             * @description Creates a new organization.
+             * @methodOf SharedFactories.OrganizationFactory
+             * @param {string}
+             *     orgName Name of the organization being created.
+             * @returns {Object} An HTTP promise.
+             */
             createOrganization: function (orgName) {
                 return $http.post(okraAPI.createOrg, {
                     organization: orgName,
                     userId: hardCoded.userId
                 });
             },
+            /**
+             * @ngdoc method
+             * @name updateMission
+             * @description Updates the mission for that organization.
+             * @methodOf SharedFactories.OrganizationFactory
+             * @param {string}
+             *     orgName Name of the organization being updated.
+             * @param {string}
+             *     mission The new mission statement for the organization.
+             * @returns {Object} An HTTP promise.
+             */
             updateMission: function (orgName, mission) {
                 var url = okraAPI.updateMission + orgName;
                 return $http.post(url, {
@@ -345,6 +374,17 @@
                     treeId: '549dcbe9efb6f7204b000001'
                 });
             },
+            /**
+             * @ngdoc method
+             * @name updateMembers
+             * @description Updates the members that are part of the organization.
+             * @methodOf SharedFactories.OrganizationFactory
+             * @param {string}
+             *     orgName Name of the organization being updated.
+             * @param {array}
+             *     members An array of objects that contain member information such as role and username.
+             * @returns {Object} An HTTP promise.
+             */
             updateMembers: function (orgName, members) {
                 var url = okraAPI.updateMembers + orgName;
                 return $http.post(url, {
