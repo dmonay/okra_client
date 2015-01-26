cd9c09a9f4d724e28b502a4d8aec621e80703d83
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-01-25 21:30:33 -0500

    implemented quick name change to objective and key result nodes

--- src/app/shared/okEditNodeDirective.js
+++ src/app/shared/okEditNodeDirective.js
@@ -17,12 +17,23 @@
      * @param {string} cancel The icon class that corresponds to the cancel/close button
      * @param {string} save The icon class that corresponds to the save button
      * @param {object} node Object to pass in that directly binds to that node (required for changing the edit mode)
+     * @param {object} parent Object to pass in that is the parent of the node being edited (optional)
+     * @param {object} tree Object that the node is attached to (required).
+     * @param {string} nodeType Type of node being edited  (required, options are KeyResult and Objective).
      *
      *
      * @usage
      *
      *  <div class="tree-node"></div>
-     *  <div layout="column" layout-align="start end" ok-edit-node edit="fa-pencil" cancel="fa-close" save="fa-check" node="objective">
+     *  <div layout="column" layout-align="start end"
+     *       ok-edit-node
+     *       edit="fa-pencil"
+     *       cancel="fa-close"
+     *       save="fa-check"
+     *       node="objective"
+     *       nodeType="Objective"
+     *       parent=""
+     *       tree="vm.tree">
      *      <md-button href ng-show="!objective.isEditMode" class="md-raised md-primary" aria-label="edit">
      *          <i class="fa fa-pencil"></i>
      *      </md-button>
@@ -35,7 +46,7 @@
      *  </div>
      */
 
-    function okEditNode() {
+    function okEditNode(TreeFactory) {
         return {
             restrict: 'A',
             link: linkFunc,
@@ -43,24 +54,48 @@
                 edit: '@',
                 cancel: '@',
                 save: '@',
-                node: '='
+                node: '=',
+                parent: '=',
+                nodeType: '@',
+                tree: '='
             }
         };
 
         function linkFunc(scope, iElement, iAttrs) {
             var saveButton;
+            var edit = false;
+
             scope.node.isEditMode = false;
 
             function switchToEdit() {
                 scope.node.newName = '';
                 scope.node.isEditMode = !scope.node.isEditMode;
-                scope.$apply();
+                if (!edit) {
+                    scope.$apply();
+                } else {
+                    edit = false;
+                }
             }
 
             function saveEdit() {
                 if (scope.node.newName.length > 0) {
                     scope.node.Name = scope.node.newName;
-                    switchToEdit();
+                    if (!scope.nodeType) {
+                        throw 'No nodeType for ok-edit-node';
+                    } else if (scope.nodeType === 'Objective') {
+                        TreeFactory.updateObjective(scope.tree, scope.node).then(function (response) {
+                            edit = true;
+                            switchToEdit();
+                        });
+                    } else if (scope.nodeType === 'KeyResult') {
+                        TreeFactory.updateKeyResult(scope.tree, scope.node, scope.parent).then(function (
+                            response) {
+                            edit = true;
+                            switchToEdit();
+                        });
+                    } else {
+                        throw 'Invalid nodeType for ok-edit-node';
+                    }
                 }
             }
 
@@ -94,7 +129,7 @@
         }
     }
 
-    // okEditNode.$inject = [];
+    okEditNode.$inject = ['TreeFactory'];
 
     app.directive('okEditNode', okEditNode);
 
