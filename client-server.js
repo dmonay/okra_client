51ba8a5a14f2594c7984e787c422e624f61ce2b0
pedromcunha <pedro.spinsomewebs@gmail.com>
2015-02-23 20:34:47 -0500

    fix to express server

--- client-server.js
+++ client-server.js
@@ -2,4 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/dist'));
 
+console.log('running on': process.env.port);
+
 app.listen(process.env.PORT || 3000);
